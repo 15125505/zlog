7c1ebe31c27d23c58f34b71e155ae8e0835fbecb
zhoufeng <zhoufeng@qsyy.com>
2016-09-07 18:20:42 +0800

    增加：加入了错误信息单独写日志文件功能。

--- log/log.go
+++ log/log.go
@@ -23,53 +23,61 @@ var LevelColor []string     // 颜色列表
 // 初始化函数
 func init() {
 	// 缺省生成一个仅仅在控制台输出的日志模块
-	Log = NewLogger(true, false, false, "")
+	Log = NewLogger(true, false, false, true, "")
 	Log.callLevel ++;
 
 	// 颜色列表
 	LevelColor = []string{
-		"\033[31m\033[40m",
-		"\033[32m\033[40m",
-		"\033[37m\033[40m",
+		"\033[31m",
+		"\033[32m",
+		"\033[37m",
 	}
 }
 
 // 日志模块
 type ZLogger struct {
-	filename    string       // 用户设定的名称
-	level       int          // 记入日志文件的等级（不低于该等级的才计入日志文件）
-	asyn        bool         // 写入文件的方式（true为异步写入）
-	fileColor   bool         // 日志文件是否写入颜色信息
-	console     bool         // 日志是否输出到控制台
-	f           *os.File     // 当前日志文件句柄
-	curFileName string       // 当前使用的文件明白
-	toWrite     chan LogNode // 需要写入文件的日志
-	callLevel   int          // 调用级别
+	prefix          string       // 用户设定的名称
+	level           int          // 记入日志文件的等级（小于等于该等级的才计入日志文件）
+	isAsynToFile    bool         // 写入文件的方式（true为异步写入）
+	isFileWithColor bool         // 日志文件是否写入颜色信息
+	isConsoleOut    bool         // 日志是否输出到控制台
+	isHaveErrFile   bool         // 日志是否加入单独的错误日志文件
+
+	pFile           *os.File     // 当前日志文件句柄
+	fileName        string       // 当前使用的文件名
+
+	pErrFile        *os.File     // 当前错误日志文件句柄
+	errFileName     string       // 当前使用的错误日志文件名
+
+	toWrite         chan LogNode // 需要写入文件的日志
+	callLevel       int          // 调用级别
 }
 
 // 创建一个日志模块（name示例："logs/myModuleName"）
 // console          -- 是否允许控制台输出
 // fileWithColor    -- 输出到文件时是否需要带上控制台颜色标志（只有在允许写文件的时候有效）
 // asynWrite        -- ture表示异步写入文件，为false表示同步写入文件（只有在允许写文件的时候有效）
+// errFile          -- ture表示为错误日志额外写入一份文件（错误日志同时也会写入普通文件），文件名如"logs/mylog-err-20160906.log"
 // fileName         -- 日志文件名，如文件名设置为"logs/mylog"，生成的日志文件将如"logs/mylog-20160906.log"，
 //                     如果fileName为空，表示日志不写入文件
-func NewLogger(console, fileWithColor, asynWrite bool, fileName string) (l *ZLogger) {
+func NewLogger(console, fileWithColor, asynWrite, errFile bool, fileName string) (l *ZLogger) {
 	l = &ZLogger{
 		callLevel:2,
 		level:LevelInformational,
 		toWrite:make(chan LogNode, maxChanCount),
 	}
-	l.Config(console, fileWithColor, asynWrite, fileName)
+	l.Config(console, fileWithColor, asynWrite, errFile, fileName)
 	go l.run()
 	return
 }
 
 // 修改配置信息
-func (l *ZLogger)Config(console, fileWithColor, asynWrite bool, fileName string) {
-	l.filename = fileName
-	l.fileColor = fileWithColor
-	l.asyn = asynWrite
-	l.console = console
+func (l *ZLogger)Config(console, fileWithColor, asynWrite, errFile bool, fileName string) {
+	l.prefix = fileName
+	l.isFileWithColor = fileWithColor
+	l.isAsynToFile = asynWrite
+	l.isHaveErrFile = errFile
+	l.isConsoleOut = console
 }
 
 // 需要写入文件的节点
@@ -96,15 +104,47 @@ func (l *ZLogger) run() {
 // 实际的写文件函数
 func (l *ZLogger) msgToFile(node LogNode) (err error) {
 
+	// 写入日志到文件
+	if l.isFileWithColor {
+		err = l.msg2File(
+			&l.pFile,
+			&l.fileName,
+			fmt.Sprintln(node.when.Format("15:04:05"), LevelColor[(node.level - LevelError) % len(LevelColor)], node.msg, "\033[0m"),
+			"",
+			node.when)
+	} else {
+		err = l.msg2File(
+			&l.pFile,
+			&l.fileName,
+			fmt.Sprintln(node.when.Format("15:04:05"), node.msg),
+			"",
+			node.when)
+	}
+
+	// 写入日志到错误日志文件
+	if l.isHaveErrFile && node.level <= LevelError {
+		err = l.msg2File(
+			&l.pErrFile,
+			&l.errFileName,
+			fmt.Sprintln(node.when.Format("15:04:05"), node.msg),
+			"-err",
+			node.when)
+	}
+
+	return
+}
+
+func (l *ZLogger)msg2File(ppFile **os.File, fileName *string, txt, tag string, when time.Time) (err error) {
+
 	// 如果文件名发生变化，需要关闭之前的文件
-	newFileName := fmt.Sprintf("%v-%v.log", l.filename, node.when.Format("20060102"))
-	if newFileName != l.curFileName && l.f != nil {
-		l.f.Close()
-		l.f = nil
+	newFileName := fmt.Sprintf("%v%v-%v.log", l.prefix, tag, when.Format("20060102"))
+	if newFileName != *fileName && *ppFile != nil {
+		(*ppFile).Close()
+		ppFile = nil
 	}
 
 	// 如果文件没有打开，首先需要打开文件
-	if nil == l.f {
+	if nil == *ppFile {
 		// 创建文件目录
 		err := os.MkdirAll(filepath.Dir(newFileName), 0600)
 		if err != nil {
@@ -117,21 +157,16 @@ func (l *ZLogger) msgToFile(node LogNode) (err error) {
 			fmt.Println("创建日志文件失败！", newFileName)
 			return err
 		}
-		l.curFileName = newFileName
-		l.f = f
+		*fileName = newFileName
+		*ppFile = f
 	}
 
 	// 写入日志到文件
-	if l.fileColor {
-		_, err = l.f.WriteString(fmt.Sprintln(node.when.Format("01-02 15:04:05"), LevelColor[(node.level - LevelError) % len(LevelColor)], node.msg, "\033[0m"))
-	} else {
-		_, err = l.f.WriteString(fmt.Sprintln(node.when.Format("01-02 15:04:05"), node.msg))
-	}
+	_, err = (*ppFile).WriteString(txt)
 	if err != nil {
 		fmt.Println("写入日志文件失败！", newFileName)
-		l.f.Close()
-		l.f = nil
-		return err
+		(*ppFile).Close()
+		*ppFile = nil
 	}
 	return
 }
@@ -152,15 +187,15 @@ func (l *ZLogger) msgOut(logLevel int, txt string) {
 	txt = "[" + filename + ":" + strconv.FormatInt(int64(line), 10) + "]" + txt
 
 	// 输出到控制台
-	if l.console {
+	if l.isConsoleOut {
 		fmt.Println(now.Format("01-02 15:04:05"), LevelColor[(logLevel - LevelError) % len(LevelColor)], txt, "\033[0m")
 	}
 
 	// 大于指定等级或者日志文件名为空，不输出到文件
-	if logLevel > l.level || l.filename == "" {
+	if logLevel > l.level || l.prefix == "" {
 		return
 	}
-	if l.asyn {
+	if l.isAsynToFile {
 		if len(l.toWrite) < maxChanCount {
 			l.toWrite <- LogNode{when:now, msg:txt, level:logLevel}
 		}
--- main.go
+++ main.go
@@ -7,16 +7,15 @@ import (
 
 func main() {
 
-	errLog := log.NewLogger(true, true, true, "")
 
-	log.Log.Config(true, true, true, "logs/abc")
+	log.Log.Config(true, true, true, true, "logs/abc")
 
 	now := time.Now()
 
 	// 用法示例
-	for i := 0; i < 600; i++ {
+	for i := 0; i < 10; i++ {
 		log.Debug(i, "哈哈哈哈哈")
-		errLog.Info(i, "hahahahhah")
+		log.Info(i, "iiiiiii")
 		log.Error(i, "fdsjaklfdjsaklfd")
 
 	}
